41810ad70c02f8092d9cddcb447ea42e624161de
HunterX-7 <fraquin7@gmail.com>
2023-02-13 19:05:59 -0500

    Create the Card for the countries in the home

---
+++ src/components/DetailCard.js
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const DetailCard = (props) => {
+  const { name, population, flag } = props;
+
+  return (
+    <>
+      <Link to={`details/${name}`}>
+        <div>
+          <img src={flag} alt={name} />
+        </div>
+      </Link>
+      <h2>{name}</h2>
+      <p>{population}</p>
+    </>
+  );
+};
+
+export default DetailCard;
+
+DetailCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  population: PropTypes.number.isRequired,
+  flag: PropTypes.string.isRequired,
+};
