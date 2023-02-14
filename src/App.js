2dff68cccd336e5b268be04379957351afbb293c
HunterX-7 <fraquin7@gmail.com>
2023-02-13 19:02:01 -0500

    Add Route and Routes for the pages

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router';
+import HomePage from './pages/HomePage';
+import DetailPage from './pages/DetailsPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/details/:name" element={<DetailPage />} />
+      <Route path="/*" element={<HomePage />} />
+    </Routes>
+  </>
+);
 
 export default App;
