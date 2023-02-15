cf12e5fa5f988b317713295a488aa534a76f8710
HunterX-7 <fraquin7@gmail.com>
2023-02-14 22:47:17 -0500

    Add bootstrap to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
